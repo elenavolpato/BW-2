6b1269e8ae92d51a6aae227c6f3c10f89ca65cd8
raposinha <elena@raposinha.dev>
2026-01-29 15:34:36 +0100

    .

--- script/home.js
+++ script/home.js
@@ -264,37 +264,6 @@ function renderCarousel(artists) {
           </div>
         
       </div>`;
-  <div class="carousel-item ${isActive}">
-    <div class="row gx-2 flex-nowrap">
-      <div class="col-md-3 g-2">
-        <a href="artist.html?id=${artists[startIndex].artist.id}" class="link-offset-2 link-underline link-underline-opacity-0">
-          <img src="${artists[startIndex].artist.picture_big}" class="w-100 card-img-top rounded-circle p-3" />
-          <h5 class="text-white text-center link-offset-2 link-underline link-underline-opacity-0">${artists[startIndex].artist.name}</h5>
-        </a>
-      </div>
-      
-      <div class="col-md-3 g-2">
-        <a href="artist.html?id=${artists[startIndex + 1].artist.id}" class="link-offset-2 link-underline link-underline-opacity-0">
-          <img src="${artists[startIndex + 1].artist.picture_big}" class="w-100 card-img-top rounded-circle p-3" />
-          <h5 class="text-white text-center link-offset-2 link-underline link-underline-opacity-0">${artists[startIndex + 1].artist.name}</h5>
-        </a>
-      </div>
-      
-      <div class="col-md-3 g-2">
-        <a href="artist.html?id=${artists[startIndex + 2].artist.id}" class="link-offset-2 link-underline link-underline-opacity-0">
-          <img src="${artists[startIndex + 2].artist.picture_big}" class="w-100 card-img-top rounded-circle p-3" />
-          <h5 class="text-white text-center link-offset-2 link-underline link-underline-opacity-0">${artists[startIndex + 2].artist.name}</h5>
-        </a>
-      </div>
-      
-      <div class="col-md-3 g-2">
-        <a href="artist.html?id=${artists[startIndex + 3].artist.id}" class="link-offset-2 link-underline link-underline-opacity-0">
-          <img src="${artists[startIndex + 3].artist.picture_big}" class="w-100 card-img-top rounded-circle p-3" />
-          <h5 class="text-white text-center link-offset-2 link-underline link-underline-opacity-0">${artists[startIndex + 3].artist.name}</h5>
-        </a>
-      </div>
-    </div>
-  </div>`;
   }
 
   // SMARTPHONE CAROUSEL
